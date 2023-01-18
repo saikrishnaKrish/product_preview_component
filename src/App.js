22e28e5527e1bc9c7c74e05fcbafe36d5392c620
unknown <sai krishna>
2023-01-18 23:24:15 +0530

    commented unnecessary import

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import CounterExample from './components/counterexample01';
-import ProductComponent from './components/product_preview_component';
+// import ProductComponent from './components/product_preview_component';
 
 function App() {
   return (
